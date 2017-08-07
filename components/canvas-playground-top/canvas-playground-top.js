758b61aa221b74a0b9c250b50c7fdc5aa0e99dcd
Dino Ivankov <dinoivankov@gmail.com>
2017-08-07 07:12:14 +0200

    Performance and memory fixes - don't log too often

--- components/canvas-playground-top/canvas-playground-top.js
+++ components/canvas-playground-top/canvas-playground-top.js
@@ -77,7 +77,7 @@ let componentData = {
             let userDataHelper = _appWrapper.getHelper('userData');
             let saved = await userDataHelper.saveUserData(appState.userData);
             if (saved && !noNotification){
-                _appWrapper.addUserMessage('User data saved.', 'info', []);
+                // _appWrapper.addUserMessage('User data saved.', 'info', []);
             }
         },
     },
